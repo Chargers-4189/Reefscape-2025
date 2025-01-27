73220f90c2cbed17d96f1834e93d9eea65254db9
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2025-01-27 15:49:12 -0500

    Updating Vision Branch (#18)
    * Added Pathfinder configuration and test path
    
    * Updated to WPI 2025.2.1
    
    * Added YAGSL drivetrain
    
    * Set robot starting position
    
    * Add pathplanner to YAGSL
    
    * Fix swerve config
    
    * Debug PID Swerve
    
    * Added subsystem discussion
    
    * Add elevator subsystem
    
    * Remove YAGSL code
    
    * Change Elevators Constants name
    
    * Add PID controller for heading
    
    * Created Coral Effector Subsystem
    
    * updated lasercan library
    
    * fixing merges (#12)
    
    * combined oi & operator constants (#13)
    
    ---------
    
    Co-authored-by: Bryan Ponce <poncebryan002@gmail.com>
    Co-authored-by: Aviator2277 <122412361+Chargers4189@users.noreply.github.com>
    Co-authored-by: Emmett <113061092+Emm128@users.noreply.github.com>
    Co-authored-by: Bryan Ponce <91227884+bponce02@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,16 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+/*
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;*/
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.DriveController;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,12 +22,14 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem swerveDrive = new DriveSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driveController = new CommandXboxController(
+    0
+  );
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,12 +48,15 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    /*
     new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
+      .onTrue(new ExampleCommand(m_exampleSubsystem));
+    */
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    swerveDrive.setDefaultCommand(
+      new DriveController(swerveDrive, driveController)
+    );
   }
 
   /**
@@ -58,6 +66,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return swerveDrive.followPathCommand("testPath");
   }
 }
