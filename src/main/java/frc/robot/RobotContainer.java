28c31f3d44d8eac13fb06ff33dd7cff604dd990a
Aviator2277 <122412361+Chargers4189@users.noreply.github.com>
2025-02-11 18:21:12 -0500

    Replace with YAGSL
    - Add YAGSL
    - Remove MAXSwerve Template
    - Replace Constants
    - Add AutoAlign
    - Add Vision & AprilTagCamera
    
    Co-Authored-By: Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,51 +4,69 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.DriveController;
+import frc.robot.commands.AutoAlign;
 import frc.robot.subsystems.CoralEffector;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.Vision;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
-  private final DriveSubsystem swerveDrive = new DriveSubsystem();
+  private final SwerveSubsystem swerve = new SwerveSubsystem();
+  private final Vision vision = new Vision();
   private final Elevator elevator = new Elevator();
   private final CoralEffector coralIntake = new CoralEffector();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController driveController = new CommandXboxController(
-    0
-  );
+      Constants.OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-    swerveDrive.setDefaultCommand(
-      new DriveController(swerveDrive, driveController)
-    );
+
+    driveController.a().onTrue(new AutoAlign(swerve, vision, false));
+    driveController.b().onTrue(new AutoAlign(swerve, vision, true));
+    driveController.start().debounce(1).onTrue(Commands.runOnce(()->{swerve.resetGyro();}, swerve));
+
+    swerve.setDefaultCommand(swerve.driveCommand(() -> driveController.getLeftY(),
+        () -> driveController.getLeftX(), () -> driveController.getRightX(), false));
   }
 
   /**
@@ -58,6 +76,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return swerveDrive.followPathCommand("testPath");
+    return new PathPlannerAuto("test-001");
   }
 }
---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,131 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.photonvision.simulation.VisionSystemSim;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.util.AprilTagCamera;
+import frc.util.AprilTagCamera.AprilTagCameraSim;
+
+public class Vision extends SubsystemBase {
+
+  private Pose2d avgEstimatedRobotPosition = new Pose2d();
+  private boolean avgEstimateAvailable = false;
+  private NetworkTableInstance networkTable = NetworkTableInstance.getDefault();
+  private StructPublisher<Pose2d> photonRobotPosition = networkTable
+      .getStructTopic("PhotonRobotPosition", Pose2d.struct).publish();
+
+  private AprilTagCamera[] cameras;
+  final Transform3d flCamPose = new Transform3d(
+      new Translation3d(0, -0.01, 0),
+      new Rotation3d(0, Math.toRadians(0), 0));
+  final Transform3d frCamPose = new Transform3d(
+      new Translation3d(0, 0, 0),
+      new Rotation3d(0, Math.toRadians(0), 0));
+  final Transform3d bkCamPose = new Transform3d(
+      new Translation3d(0, 0.01, 0),
+      new Rotation3d(0, Math.toRadians(0), 0));
+
+  // ----- Simulated Vision -----
+  VisionSystemSim visionSim;
+
+  /** Creates a new vision. */
+  public Vision() {
+    if (Robot.isSimulation()) {
+      setupSimCameras();
+      System.out.println("SIM");
+    } else {
+      cameras = new AprilTagCamera[] {
+          new AprilTagCamera("flCam2025", flCamPose),
+          new AprilTagCamera("frCam2025", frCamPose),
+          new AprilTagCamera("bkCam2025", bkCamPose),
+      };
+    }
+  }
+
+  public void setupSimCameras() {
+    visionSim = new VisionSystemSim("main");
+    try {
+      AprilTagFieldLayout tagLayout = AprilTagFieldLayout
+          .loadFromResource(AprilTagFields.k2025Reefscape.m_resourceFile);
+      visionSim.addAprilTags(tagLayout);
+    } catch (Exception e) {
+      System.out.println("Could not load simulated field: " + e);
+    }
+    cameras = new AprilTagCamera[] {
+        new AprilTagCameraSim("flCam2025", flCamPose, true, visionSim),
+        new AprilTagCameraSim("frCam2025", frCamPose, false, visionSim),
+        new AprilTagCameraSim("bkCam2025", bkCamPose, false, visionSim),
+    };
+  }
+
+  public Double getFrontLeftTagYaw() {
+    if (cameras[0].getEstimatedTagYaw() != null) {
+      return cameras[0].getEstimatedTagYaw();
+    } else {
+      return null;
+    }
+  }
+
+  public Double getFrontRightTagYaw() {
+    if (cameras[1].getEstimatedTagYaw() != null) {
+      return cameras[1].getEstimatedTagYaw();
+    } else {
+      return null;
+    }
+  }
+
+  public Pose2d getEstimatedRobotPosition() {
+    if (avgEstimateAvailable) {
+      return avgEstimatedRobotPosition;
+    }
+    return null;
+  }
+
+  public void AvgEstimatedRobotPosition() {
+    int count = 0;
+    double x = 0;
+    double y = 0;
+    double degrees = 0;
+    for (AprilTagCamera camera : cameras) {
+      if (camera.isEstimateReady()) {
+        x += camera.getEstimatedRobotPose().getX();
+        y += camera.getEstimatedRobotPose().getY();
+        degrees += camera.getEstimatedRobotPose().getRotation().getDegrees();
+        count++;
+      }
+    }
+    x /= count;
+    y /= count;
+    degrees = ((degrees / count) * Math.PI) / 180;
+    avgEstimatedRobotPosition = new Pose2d(x, y, new Rotation2d(degrees));
+    avgEstimateAvailable = true;
+    if (count == 0) {
+      avgEstimatedRobotPosition = null;
+      avgEstimateAvailable = false;
+    }
+  }
+
+  @Override
+  public void periodic() {
+    for (AprilTagCamera camera : cameras) {
+      camera.update();
+    }
+    AvgEstimatedRobotPosition();
+    photonRobotPosition.set(avgEstimatedRobotPosition);
+    System.out.println(cameras[0].getEstimatedTagYaw());
+  }
+}
