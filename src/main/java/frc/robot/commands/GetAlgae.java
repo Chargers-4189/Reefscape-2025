6b9263c1850a303249c9f1aae2180a310f8f18de
Nightowl5275 <139814989+Nightowl5275@users.noreply.github.com>
2025-02-16 19:33:54 -0500

    Add get algae

--- src/main/java/frc/robot/commands/GetAlgae.java
+++ src/main/java/frc/robot/commands/GetAlgae.java
@@ -5,12 +5,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralEffector;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class GetAlgae extends Command {
+    private CoralEffector coraleffector;
+
+
   /** Creates a new GetAlgae. */
-  public GetAlgae() {
+  public GetAlgae(CoralEffector coraleffector) {
+    this.coraleffector = coraleffector;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(coraleffector);
   }
 
   // Called when the command is initially scheduled.
@@ -19,11 +25,15 @@ public class GetAlgae extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    coraleffector.intakeAlgae(0.1);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    coraleffector.stop();
+  }
 
   // Returns true when the command should end.
   @Override
