34eb38d3dd9ad319ec9af7fdd15bd1037a1764fb
Aviator2277 <122412361+Chargers4189@users.noreply.github.com>
2025-03-06 21:24:56 -0500

    Decreased drive speed to prevent tipping
    Co-Authored-By: Emm128 <113061092+Emm128@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -208,7 +208,7 @@ public final class Constants {
   }
   
   public static class HumanDriveConstants {
-    public static final double kDRIVE_POWER = .8;
+    public static final double kDRIVE_POWER = .4; //Decreased for safety: prevent tipping
     public static final double kROTATIONAL_POWER = .8;
     public static final double kDRIVE_EXPONENT = 3;
     public static final double kROTATIONAL_EXPONENT = 1;
