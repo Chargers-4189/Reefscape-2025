f7540668aacb9ac691f5c8bd30b56dd2ef41f33d
Aviator2277 <122412361+Chargers4189@users.noreply.github.com>
2025-03-08 08:09:31 -0500

    tagAngles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -178,6 +178,8 @@ public final class Constants {
     public static final double kDIST_FROM_REEF = Units.inchesToMeters(0);
     public static final double kDIST_OFFSET = Units.inchesToMeters(11.338);
 
+    public static final int[] kTagAngles = {30};
+
     /* 
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared =
