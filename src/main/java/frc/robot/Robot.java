d9d043f8247bea6ac2692856e6a1b70e2a447426
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2026-01-14 18:51:38 -0500

    Delete code files for students

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,10 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.util.Elastic;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -22,8 +20,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
-  private Timer disabledTimer;
 
   public Robot() {
     instance = this;
@@ -44,9 +40,6 @@ public class Robot extends TimedRobot {
 
     // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop
     // immediately when disabled, but then also let it be pushed more
-    disabledTimer = new Timer();
-
-    Elastic.initialize();
 
     if (isSimulation()) {
       DriverStation.silenceJoystickConnectionWarning(true);
@@ -74,21 +67,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void disabledInit() {
-    m_robotContainer.setMotorBrake(true);
-    disabledTimer.reset();
-    disabledTimer.start();
   }
 
   @Override
   public void disabledPeriodic() {
-    m_robotContainer.face();
-    if (
-      disabledTimer.hasElapsed(Constants.DrivebaseConstants.WHEEL_LOCK_TIME)
-    ) {
-      m_robotContainer.setMotorBrake(false);
-      disabledTimer.stop();
-      disabledTimer.reset();
-    }
   }
 
   /**
@@ -96,7 +78,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_robotContainer.setMotorBrake(true);
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
