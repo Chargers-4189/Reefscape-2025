2fd2f09517cfbb92765589788db698d54d66a283
Lynn Mortensen <lynnrm08@gmail.com>
2026-02-04 16:52:37 -0500

    e

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -6,15 +6,10 @@ package frc.robot.subsystems;
 
 import java.util.List;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import org.photonvision.targeting.TargetCorner;
 
-import edu.wpi.first.apriltag.AprilTag;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
@@ -23,16 +18,25 @@ public class Vision extends SubsystemBase {
   PhotonCamera cam = new PhotonCamera("frCam2025");
 
   public Vision() {
-    //var result = cam.getLatestResult();
+
     //boolean hasTragets;
-    System.out.println(cam.getLatestResult());
+    
   }
 
+  /*public PhotonTrackedTarget bestResult(){
+    var result = cam.getLatestResult();
+    List <PhotonTrackedTarget> targets = result.getTargets();
+    
+  } */
+
+  public void poseEstmater(){
+
+  }  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    var result = cam.getLatestResult();
+    /*var result = cam.getLatestResult();
     boolean hasTragets = result.hasTargets();
     System.out.println("isHasTarget: " + hasTragets);
     if(hasTragets){
@@ -50,10 +54,13 @@ public class Vision extends SubsystemBase {
       Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
 
 
-      System.out.println("way: " + yaw + " pitch: " + pitch + " area: " + area + " ID: " + targetID + " ambig: " +  poseAmbiguity + " bestCameraToTarget: " + bestCameraToTarget + " alternateCameraToTarget: " + alternateCameraToTarget);
+      
 
     }else{
       System.out.println("there is no target");
-    }
+    } */
+
+    System.out.println(cam.getAllUnreadResults());
+
   }
 }
---
+++ src/main/java/frc/util/Camera.java
@@ -0,0 +1,14 @@
+package frc.util;
+
+
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import frc.robot.subsystems.Vision;
+
+public class Camera {
+   /* public PhotonPipelineResult getBestResult(Vision vis){
+
+    return ;
+   } */
+}
